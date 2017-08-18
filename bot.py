2246fcb006100b0aee3c7e3460369c6aae0e5366
Alexandre CHENIEUX <alexandre.chenieux@free.fr>
2017-08-18 17:39:58 -0400

    test opus

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from bot.DiscordClient import DiscordClient
 
 if __name__ == '__main__':
     if not discord.opus.is_loaded():
-        discord.opus.load_opus('/tmp/opus-build/opus_sm-master/libopus.la')
+        discord.opus.load_opus('/tmp/opus-build/opus_sm-master/libopus.so')
 
     client = DiscordClient()
     client.run(os.environ.get('BOT_TOKEN'))
